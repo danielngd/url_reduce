9e57774fa4da6bcd3780e2753350597c6daf0408
Daniel Lima <daniel.ngd@gmail.com>
2021-05-22 21:52:36 -0300

    correção dos redirecionamentos

--- url_reduce/urls.py
+++ url_reduce/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from url_reducer import views
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<slug:slug>', views.redirecionar),
+    path('relatorios/<slug:slug>', views.relatorios),
 ]
--- url_reducer/views.py
+++ url_reducer/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 # Create your views here.
+from url_reducer.models import UrlRedirect
+
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+        }
+    return render(requisicao, 'url_reducer/relatorio.html', contexto)
+
 
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
