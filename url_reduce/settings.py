d678ebbf9dc7bccec5ad64539b0dd259907528b7
Daniel Lima <daniel.ngd@gmail.com>
2021-05-24 16:45:55 -0300

    secret key

--- url_reduce/settings.py
+++ url_reduce/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'secure-sea-21927.herokuapp.com']
 
 
