662119546c99a4a0e3e91fe45b37a1b6bd841c29
Daniel Lima <daniel.ngd@gmail.com>
2021-05-24 16:32:52 -0300

    Adicionando arquivo para local config

--- url_reduce/settings.py
+++ url_reduce/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+&@q0*icl!iek(z=-m$3jtsus-p@g3k&(6!8_0^rgr@-^e&)s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -153,3 +151,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
